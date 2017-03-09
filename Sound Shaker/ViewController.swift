2253c0bd8fd5a521e594a70052395a0d97786b10
Liz Healy <lizhealy@Lizs-MacBook-Pro.local>
2017-03-09 10:25:33 -0500

    Initial Commit

--- Sound Shaker/ViewController.swift
+++ Sound Shaker/ViewController.swift
@@ -7,13 +7,37 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player = AVAudioPlayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+            }
+    
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        if event?.subtype == UIEventSubtype.motionShake {
+
+            var randomNumber = Int(arc4random_uniform(5)) + 1
+            
+            let audioPath = Bundle.main.path(forResource: "sound\(randomNumber)", ofType: "mp3")
+            do {
+                try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
+            }
+            catch {
+                //errors
+            }
+
+            player.play()
+            print(randomNumber)
+
+        }
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
